3f7e3907f35694cbfddfd96a5726be7dcfc78a7d
calengil <150662304+calengil@users.noreply.github.com>
2024-01-10 13:35:41 +0700

    Update decode_to_bed.py

--- decode_to_bed.py
+++ decode_to_bed.py
@@ -2,62 +2,49 @@
 
 import argparse
 
+import h5py
+import numpy as np
 import pandas as pd
 from transformers import AutoModel, AutoTokenizer
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--tokenized_seqs", type=str, help="Path to TXT file")
+parser.add_argument("--targets_file", type=str, help="Path to HDF5 file")
 args = parser.parse_args()
-
-data = pd.read_csv(args.tokenized_seqs, sep="\t", header=None)
 tokenizer = AutoTokenizer.from_pretrained("AIRI-Institute/gena-lm-bert-large-t2t")
+class_lables = np.array(["5UTR", "exon", "intron", "3UTR", "CDS", "intergenic region"])
 
 
-def split_fields(info_record):
-    records = info_record[1:-1].split(", ")
-    return records
-
-
-def to_chars(info_record):
-    records = tokenizer.convert_ids_to_tokens(info_record)
-    return records
-
-
-data["class_0"] = data[10].apply(split_fields)
-data["class_1"] = data[11].apply(split_fields)
-data["class_2"] = data[12].apply(split_fields)
-data["class_3"] = data[13].apply(split_fields)
-data["class_4"] = data[14].apply(split_fields)
-data["class_5"] = data[15].apply(split_fields)
-
-data["ids"] = data[7].apply(split_fields)
-data["chars"] = data["ids"].apply(to_chars)
-
-class_lables = ["5UTR", "exon", "intron", "3UTR", "CDS", "intergenic region"]
-
-
-def main(df):
-    start = int(df[5])
-    for i, seq in enumerate(df["chars"]):
-        elem = []
-        classes = [
-            df["class_0"][i],
-            df["class_1"][i],
-            df["class_2"][i],
-            df["class_3"][i],
-            df["class_4"][i],
-            df["class_5"][i],
-        ]
-        for ind, stat in enumerate(classes):
-            if stat == "1":
-                elem.append(class_lables[ind])
-        if len(elem) == 1 and elem[0] == "intergenic region":
-            print(f"{df[3]}\t{start}\t{start + len(seq)}\t{';'.join(map(str, elem))}")
-        else:
-            print(
-                f"{df[3]}\t{start}\t{start + len(seq)}\t{';'.join(map(str, elem))};gene={df[0]};transcript={df[1]};class={df[2]};strand={df[4]};seq={seq}"
+with h5py.File(args.targets_file, "r") as file:
+    for transcript in list(file["records"].keys()):
+        for sample in list(file["records"][transcript].keys()):
+            token_ids_record = np.array(
+                file["records"][transcript][sample]["token_ids"]
             )
-        start += len(seq)
+            chars = tokenizer.convert_ids_to_tokens(token_ids_record)
 
+            sample_start = np.array(
+                file["records"][transcript][sample]["coordinates"][0]
+            )
+            sample_end = np.array(
+                file["records"][transcript][sample]["coordinates"][-1]
+            )
 
-data.apply(main, axis=1)
+            classes = np.array(file["records"][transcript][sample]["classes"])
+
+            chr = str(file["records"][transcript][sample]["info"][0]).split("'")[1]
+            gene = str(file["records"][transcript][sample]["info"][1]).split("'")[1]
+            trans = str(file["records"][transcript][sample]["info"][2]).split("'")[1]
+            trans_class = str(file["records"][transcript][sample]["info"][3]).split(
+                "'"
+            )[1]
+            strand = str(file["records"][transcript][sample]["info"][4]).split("'")[1]
+
+            start = int(sample_start)
+            for i, seq in enumerate(chars[1:-1]):
+                column = np.array(classes[:, i+1])
+                column_mask = (column > 0)
+
+                print(
+                    f"{chr}\t{start}\t{start + len(seq)}\t{class_lables[column_mask]};gene={gene};transcript={trans};class={trans_class};strand={strand};seq={seq}"
+                )
+                start += len(seq)
